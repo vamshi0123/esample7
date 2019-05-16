6c8a9d4cad35f15d23f448eba1c6afab36f81a35
vamshi <vamshi.doc001@gmail.com>
2019-05-16 16:31:40 +0530

    this is sample 7

---
+++ src/test/java/sample7/sample7test.java
@@ -0,0 +1,19 @@
+package sample7;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class sample7test {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
